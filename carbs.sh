0626996cb40d21db62ebb5f14c1480f3ac874d7c
Chen Jicheng <git@chenjicheng.com>
2022-05-17 01:59:26 +0800

    Temporary Solutions
    https://sourceforge.net/p/veracrypt/discussion/general/thread/a833a53acc/

--- carbs.sh
+++ carbs.sh
@@ -245,6 +245,15 @@ GLFW_IM_MODULE=ibus\n" >> /etc/environment
 	sed -i 's/arch.pool/pool/g' /etc/ntp.conf
 	sed -i '/[0-9].pool/s/$/ iburst/' /etc/ntp.conf
 
+	# Temporary Solutions
+	printf "polkit.addRule(function(action, subject) {
+        if (((action.id == "org.freedesktop.udisks2.filesystem-fstab") ||
+            (action.id == "org.freedesktop.udisks2.filesystem-mount-system")) &&
+            subject.local && subject.active) {
+            return polkit.Result.YES;
+        }
+});\n" > /etc/polkit-1/rules.d/nopasswd.rules
+
 	# usermod
 	[[ -z $(command -v wireshark) ]] && usermod -aG wireshark $name
 
